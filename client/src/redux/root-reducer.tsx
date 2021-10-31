53bae68bf2cc2c44798ccc6fc7d22fe67137dd50
Krasimira Stankova <kstankova@astea.net>
2021-10-31 22:55:23 +0200

    initial fix in redux setup; added lower version of history package

--- client/src/redux/root-reducer.tsx
+++ client/src/redux/root-reducer.tsx
@@ -2,14 +2,23 @@ import { connectRouter, RouterState } from 'connected-react-router';
 import { combineReducers } from 'redux';
 import modalReducer from './modal-visibility/modal.reducer';
 import { ModalState } from './modal-visibility/modal.types';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { PersistPartial } from 'redux-persist/lib/persistReducer';
 
 export interface StoreState {
-    router: RouterState;
+    router: RouterState & PersistPartial;
     modal: ModalState;
 };
 
+const routerConfig = {
+    key: 'router',
+    storage: storage,
+    whitelist: ['router']
+}
+
 export const rootReducer = (history: any) => combineReducers<StoreState>({
-    router: connectRouter(history),
+    router: persistReducer(routerConfig, connectRouter(history)),
     modal: modalReducer,
 });
 
