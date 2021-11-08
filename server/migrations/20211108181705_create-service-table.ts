86a8d45f5844823e756a51cdc5cd27fbb87ae709
sharkoo0 <ssharko123@gmail.com>
2021-11-08 20:28:55 +0200

    Service table migration created

---
+++ server/migrations/20211108181705_create-service-table.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("service", (table: Knex.TableBuilder) => {
+        table.increments("service_id");
+        table.double("price").notNullable();
+        table.string("name").notNullable();
+        table.string("description").notNullable();
+        table.double("discount").defaultTo(0);
+        table.string("category").notNullable(); //table.enum(...) може да го направим с enum и просто да имаме няколко варианта
+        table.string("duration").notNullable();
+        table.string("city").notNullable(); // better will be 'region'
+        table.string("picture");
+        table.integer("contributor_id").notNullable();
+        table.float("rating").notNullable().defaultTo(0);
+        table.date("publish_date").notNullable();
+      });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("service");
+}
+
