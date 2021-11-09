66268b77df324178817e6d9df5929cc81a65f620
milicasg <milicasgp@gmail.com>
2021-11-09 20:51:11 +0200

    Some fixes

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -49,4 +49,60 @@ const deleteUser = async (req: express.Request, res: express.Response) => {
     }
 }
 
-export {changePassword, editInfo, deleteUser};
+
+const register = async (req: express.Request, res: express.Response) => {
+    try {
+        const user: User = req.body;
+        user.password = hashPassword(user.password);
+
+        await User.query()
+            .insert(user);
+
+        return res.status(201).json("Successful registration");
+
+    } catch (err) {
+        console.log(err);
+        return res.status(422).json("Registration failed:" + err);
+    }
+}
+
+const registerWithGoogle = async (req: express.Request, res: express.Response) => {
+    try {
+        const googleUser: User = req.body;
+        googleUser.password = googleUser.email;
+        req.body = googleUser;
+
+        return register(req, res);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(422).json(err);
+    }
+}
+
+//========= FRONT-END: GOOGLE SING IN/OUT ============
+// google sign out
+export function onSignIn(googleUser: any) {
+    let profile = googleUser.getBasicProfile();
+    const googleUserToSend = {
+        "email": profile.getEmail(), //can be null
+        "role": "",//TO SET
+        "name": profile.getName(),
+        "profile_pic": profile.getImageUrl() ? profile.getImageUrl() : null,
+    }
+
+    return googleUserToSend;
+}
+
+export function signOut() {
+    gapi.auth2.getAuthInstance().signOut().then(() => {
+        console.log('User signed out.');
+    });
+}
+//=========================
+
+const hashPassword = (pass: string) => {
+    return bcrypt.hashSync(pass, SALT_ROUNDS)
+}
+
+export { changePassword, editInfo, deleteUser, register, registerWithGoogle };
