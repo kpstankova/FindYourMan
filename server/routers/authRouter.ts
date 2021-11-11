84dbb2dd9c095d6d10ab65c16e12517ef7def022
milicasg <milicasgp@gmail.com>
2021-11-11 16:39:47 +0200

    Google SSO fixed.

--- server/routers/authRouter.ts
+++ server/routers/authRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { changePassword, editInfo, deleteUser,register, loginWithGoogle, googleSignOut} from '../controllers/authController';
+import { changePassword, editInfo, deleteUser, register, loginWithGoogle, googleSignOut } from '../controllers/authController';
 
 const authRouter = express.Router();
 
@@ -8,6 +8,6 @@ authRouter.put('/editInfo', editInfo);
 authRouter.delete('/deleteUser', deleteUser);
 
 authRouter.post('/register', register);
-authRouter.post('/login/google',loginWithGoogle)
-authRouter.post('/logout/google',googleSignOut);
+authRouter.post('/login/google', loginWithGoogle)
+authRouter.post('/logout/google', googleSignOut);
 export default authRouter;
