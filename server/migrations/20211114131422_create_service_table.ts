126389cb36d3644ccdc528d5ea7676c614f6bb55
sharkoo0 <ssharko123@gmail.com>
2021-11-14 15:26:45 +0200

    Fix migration bug, set constraints for models and tables
    Изтрийте старата миграция, която имате, преди да pull-нете, тъй като са напълно нови файлове.

--- server/migrations/20211114131422_create_service_table.ts
+++ server/migrations/20211114131422_create_service_table.ts
@@ -1,26 +1,22 @@
 import { Knex } from "knex";
-import User from '../models/User';
-
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("service", (table: Knex.TableBuilder) => {
         table.increments("service_id");
-        table.double("price").notNullable();
         table.string("name").notNullable();
         table.string("description").notNullable();
-        table.double("discount").defaultTo(0);
         table.enum("category", ["Programming", "Design", "Cooking", "Mechanics", "Maths", "Entertainment"]).notNullable();
         table.string("duration").notNullable();
         table.string("city").notNullable(); // better will be 'region'
-        table.string("picture");
         table.integer("contributor_id").unsigned().notNullable().references("user_id").inTable("user");
+        table.double("price").notNullable();
+        table.timestamp("publish_date").notNullable().defaultTo(knex.fn.now());    
+        table.double("discount").defaultTo(0);        
+        table.string("picture");  
         table.float("rating").notNullable().defaultTo(0);
-        table.timestamp("publish_date").notNullable().defaultTo(knex.fn.now());
       });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTable("service");
 }
-
--- server/models/Service.ts
+++ server/models/Service.ts
@@ -3,17 +3,17 @@ import User from './User';
 
 export default class Service extends Model {
     service_id: number;
-    price: number;
     name: string;
     description: string;
-    discount?: number;
     category: ["Programming", "Design", "Cooking", "Mechanics", "Maths", "Entertainment"];
     duration: string;
     city: string;
-    picture?: string;
     contributor_id: number;
+    price: number;
+    publish_date: Date; 
+    discount?: number; 
+    picture?: string;
     rating?: number;
-    publish_date: Date;
 
     static tableName = "service";
 
@@ -22,17 +22,22 @@ export default class Service extends Model {
         required: ["price", "name", "description", "category", "duration", "city", "contributor_id"],
         properties: {
             service_id: {type: "integer"},
-            price: {type: ["float", "0"]},
-            name: {type: "string", minLength: 1, maxLength: 50},
-            description: {type: "string", minLength: 1, maxLength: 500},
-            discount: {type: ["float", "0"]},
-            category: {type: "string"},
+            name: {type: "string", 
+                    minLength: 1, 
+                    maxLength: 50},
+            description: {type: "string", 
+                            minLength: 1, 
+                            maxLength: 500},
             duration: {type: "string"},
+            category: {type: "string", 
+                        enum: ["Programming", "Design", "Cooking", "Mechanics", "Maths", "Entertainment"]},
             city: {type: "string"},
-            picture: {type: ["string", "null"]},
             contributor_id: {type: "integer"},
+            price: {type: ["float", "0"]},           
+            publish_date: {type: "date"},          
+            discount: {type: ["float", "0"]},          
+            picture: {type: ["string", "null"]},          
             rating: {type: ["float", "0"]},
-            publish_date: {type: "date"},
         }
     }
 
