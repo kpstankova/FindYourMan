e0b8412d018a654c882cd262807958f4c10b3790
sharkoo0 <ssharko123@gmail.com>
2022-01-02 21:41:45 +0200

    Upload files in progress

--- server/controllers/filesController.ts
+++ server/controllers/filesController.ts
@@ -1,5 +1,24 @@
 import express from 'express';
+import Multer from 'multer';
 import { createProfileFolder } from '../utils/fileUtils';
+
+function storageUpload() {
+    const storage = Multer.diskStorage({
+        destination: (req, file, cb) => {
+        cb(null, '../images/');
+        },
+        filename: (req, file, cb) => {
+        cb(null, file.originalname);
+        },
+    });
+    return storage;
+}
+
+let Upload = Multer({
+    storage: storageUpload(),
+});
+
+
 
 const createFolder = async (req: express.Request, res: express.Response) => {
     console.log(req.body.folder);
@@ -7,4 +26,9 @@ const createFolder = async (req: express.Request, res: express.Response) => {
     res.json(result);
 }
 
-export {createFolder}
+const uploadFiles = async (req: express.Request, res: express.Request) => {
+    const newFiles = req.files;
+    
+}
+
+export {createFolder, uploadFiles};
