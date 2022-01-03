bfa866ad2f05724233929128fe0426932560ae00
milicasg <milicasgp@gmail.com>
2022-01-03 04:12:31 +0200

    Get_system debit() from date parameter added

--- server/migrations/storedProcedures/get_system_debit_for_period.sql
+++ server/migrations/storedProcedures/get_system_debit_for_period.sql
@@ -1,8 +1,8 @@
-CREATE DEFINER=`YOUR_PASS_HERE`@`localhost` PROCEDURE `get_system_debit_for_period`(in months integer)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_system_debit_for_period`(in from_date date, in period_in_months integer)
 BEGIN
 	SELECT SUM(AMOUNT) 
 	FROM TRANSACTION
 	WHERE DEBIT = 'SYSTEM_IBAN'
 	AND TIMESTAMP BETWEEN CURRENT_TIMESTAMP() 
-    AND DATE_SUB(current_timestamp(), INTERVAL months MONTH);
+    AND DATE_SUB(from_date, INTERVAL period_in_months MONTH);
 END
